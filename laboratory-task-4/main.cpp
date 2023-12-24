ae9e1cf26e6ea9bcf024e06f54bb7c4e09db5fbd
DaniilRubets <smyralevka2020@gmail.com>
2023-12-24 16:52:00 +0300

    Добавил условие

--- laboratory-task-4/main.cpp
+++ laboratory-task-4/main.cpp
@@ -1,5 +1,7 @@
 /*
-
+    Заполнить квадратную матрицу последовательными натуральными числами,
+    расположенными по спирали, начиная с левого верхнего угла и продвигаясь по часовой стрелке.
+    Найти сумму элементов побочной диагонали.
 */
 
 
@@ -13,10 +15,8 @@ using std::cout;
 
 void fillingWithZeroes(int32_t** matrix, int32_t& rows,int32_t &columns)
 {
-	for (size_t i = 0; i < rows; i++)
-	{
-		for (size_t j = 0; j < columns; j++)
-		{
+	for (size_t i = 0; i < rows; i++) {
+		for (size_t j = 0; j < columns; j++) {
 			matrix[i][j] = 0;
 		}
 	}
@@ -28,8 +28,7 @@ void inputStartValueFromKeyboard(int32_t& value)
     cout << "Input start value:";
     cin >> value;
 
-    if (value <= 0)
-    {
+    if (value <= 0) {
         cout << "Please, input natural number!";
         cin >> value;
     }
@@ -43,16 +42,14 @@ void inputStartValueFromRandom(int32_t &value)
     cout << "Input right boader:";
     cin >> rightBoader;
 
-    if (rightBoader < 1)
-    {
+    if (rightBoader < 1) {
         cout << "Please, input natural number!";
         cin >> rightBoader;
     }
     cout << "Input left boader:";
     cin >> leftBoader;
 
-    if (leftBoader < 1)
-    {
+    if (leftBoader < 1) {
         cout << "Please, input natural number!";
         cin >> leftBoader;
     }
@@ -62,69 +59,56 @@ void inputStartValueFromRandom(int32_t &value)
 
 void spiralFilling(int32_t** matrix, int32_t& rows, int32_t& columns, int32_t &value)
 {
-    int32_t column_p = 0, row_p = 0, direction = 0;
+    int32_t columnPos = 0, rowPos = 0, direction = 0;
 
-    while (1) 
-    {
-        matrix[row_p][column_p] = value;
+    while (1) {
+        matrix[rowPos][columnPos] = value;
         ++value;
 
-        if (row_p == rows / 2 && column_p == columns / 2)
-        {
-            matrix[row_p][column_p] = value;
+        if (rowPos == rows / 2 && columnPos == columns / 2) {
+            matrix[rowPos][columnPos] = value;
             break;
         }
 
-        if (direction == 0) 
-        {
-            ++column_p;
+        if (direction == 0) {
+            ++columnPos;
 
-            if (column_p == columns || matrix[row_p][column_p] != 0) 
-            {
-                --column_p;
-                ++row_p;
+            if (columnPos == columns || matrix[rowPos][columnPos] != 0) {
+                --columnPos;
+                ++rowPos;
                 direction = 1;
             }
         }
 
-        else 
-        {
-            if (direction == 1)
-            {
-                ++row_p;
+        else {
+            if (direction == 1) {
+                ++rowPos;
 
-                if (row_p == rows || matrix[row_p][column_p] != 0) 
-                {
-                    --row_p;
-                    --column_p;
+                if (rowPos == rows || matrix[rowPos][columnPos] != 0) {
+                    --rowPos;
+                    --columnPos;
                     direction = 2;
                 }
             }
 
-            else 
-            {
-                if (direction == 2) 
-                {
-                    --column_p;
+            else {
+                if (direction == 2) {
+                    --columnPos;
 
-                    if (column_p == -1 || matrix[row_p][column_p] != 0) 
-                    {
-                        ++column_p;
-                        --row_p;
+                    if (columnPos == -1 || matrix[rowPos][columnPos] != 0) {
+                        ++columnPos;
+                        --rowPos;
                         direction = 3;
                     }
                 }
 
-                else 
-                {
-                    if (direction == 3) 
-                    {
-                        --row_p;
+                else {
+                    if (direction == 3) {
+                        --rowPos;
 
-                        if (row_p == -1 || matrix[row_p][column_p] != 0) 
-                        {
-                            ++row_p;
-                            ++column_p;
+                        if (rowPos == -1 || matrix[rowPos][columnPos] != 0) {
+                            ++rowPos;
+                            ++columnPos;
                             direction = 0;
                         }
                     }
@@ -138,12 +122,12 @@ void spiralFilling(int32_t** matrix, int32_t& rows, int32_t& columns, int32_t &v
 void sumOfSecondaryDiagonal(int32_t** matrix, int32_t& value, int32_t& rows, int32_t& columns)
 {
     int32_t sum = 0;
-    for (size_t i = 0; i < rows; i++)
-    {
-        for (size_t j = 0; j < columns; j++)
-        {
-            if ((i + j) == (rows - 1))
-            {
+    for (size_t i = 0; i < rows; i++) {
+
+        for (size_t j = 0; j < columns; j++) {
+
+            if ((i + j) == (rows - 1)) {
+
                 sum += matrix[i][j];
             }
         }
@@ -154,10 +138,10 @@ void sumOfSecondaryDiagonal(int32_t** matrix, int32_t& value, int32_t& rows, int
 
 void printingMatrix(int32_t** matrix, int32_t &rows, int32_t &columns)
 {
-	for (size_t i = 0; i < rows; i++)
-	{
-		for (size_t j = 0; j < columns; j++)
-		{
+	for (size_t i = 0; i < rows; i++) {
+
+		for (size_t j = 0; j < columns; j++) {
+
             cout << std::setw(5) << matrix[i][j] << " ";
 		}
 		cout << '\n';
@@ -170,13 +154,16 @@ void switchMode(int32_t& value, int32_t& mode)
     cout << "Chooze mode of input start value"<<'\n'<<"1.Input from keyboard"<<'\n'<<"2.Input from random"<<'\n';
     cin >> mode;
 
-    switch (mode)
-    {
+    switch (mode) {
+
     case 1:
         inputStartValueFromKeyboard(value);
         break;
+
     case 2:
         inputStartValueFromRandom(value);
+        break;
+
     default:
         cout << "Please, input good number";
         cin >> mode;
@@ -195,16 +182,14 @@ int main()
         cout << "Input size of your matrix:";
         cin >> rows;
 
-        if (rows <= 0)
-        {
+        if (rows <= 0) {
             cout << "Please, input natural number!";
             cin >> rows;
         }
         int32_t columns = rows;
         int32_t** matrix = new int*[rows];
 
-        for (size_t i = 0; i < rows; i++)
-        {
+        for (size_t i = 0; i < rows; i++) {
             matrix[i] = new int[columns];
         }
         fillingWithZeroes(matrix, rows, columns);
@@ -212,12 +197,12 @@ int main()
         spiralFilling(matrix, rows, columns,value);
         printingMatrix(matrix ,rows,columns);
         sumOfSecondaryDiagonal(matrix, value, rows, columns);
-        for (size_t i = 0; i < rows; i++)
-	    {
+        for (size_t i = 0; i < rows; i++) {
 		    delete[]matrix[i];
 	    }
 	        delete[]matrix;
     }
+
     catch(std::exception e) {
         cout << e.what();
     }
