2f6ceefc8a01ac7a142813376be07816dc53890b
DaniilRubets <smyralevka2020@gmail.com>
2024-01-07 02:42:55 +0300

    Теперь функция проверки строки выполняет одно действие, функция поиска уникальных символов возвращает значение, добавил отступ.

--- laboratory-task-8/main.cpp
+++ laboratory-task-8/main.cpp
@@ -1,8 +1,8 @@
 /*
-Строка состоит из слов, разделённых знаками препинания. 
-За один просмотр символов строки найти все слова, содержащие наибольшее количество различных символов, и занести их в новую строку. 
-Слова в новой строке должны разделяться ровно одним пробелом. 
-Решение задачи получить за один просмотр символов строки.
+    Строка состоит из слов, разделённых знаками препинания. 
+    За один просмотр символов строки найти все слова, содержащие наибольшее количество различных символов, и занести их в новую строку. 
+    Слова в новой строке должны разделяться ровно одним пробелом. 
+    Решение задачи получить за один просмотр символов строки.
 */
 
 
@@ -10,19 +10,16 @@
 #include <string>
 
 
-void inputAndCheckString(std::string& str)
+void checkString(std::string& str)
 {
-    std::cout << "Input your string: ";
-    std::getline(std::cin, str);
-
     if (str.empty() == true) {
         std::cout << "Your string is empty!";
         std::getline(std::cin, str);
     }
- }
+}
 
 
-void findMaxUnigueChars(std::string& str, std::string& word, std::string& maxWord, size_t& maxUnigueChars, size_t& start, size_t& end)
+std::string findMaxUnigueChars(std::string& str, std::string& word, std::string& maxWord, size_t& maxUnigueChars, size_t& start, size_t& end)
 {
     while (end != std::string::npos) {
         word = str.substr(start, end - start);
@@ -43,7 +40,7 @@ void findMaxUnigueChars(std::string& str, std::string& word, std::string& maxWor
         end = str.find(' ', start);
     }
 
-    std::cout << "This is your word: " << maxWord << std::endl;
+    return maxWord;
 }
 
 
@@ -54,15 +51,17 @@ int main()
         std::string delimiters = " .,-:)(!?;";
         std::string word = "";
         std::string maxWord = "";
-        inputAndCheckString(str);
+        std::cout << "Input your string: ";
+        std::getline(std::cin, str);
+        checkString(str);
         size_t maxUnigueChars = 0;
         size_t start = 0;
         size_t end = str.find_first_of(delimiters);
-        findMaxUnigueChars(str, word, maxWord, maxUnigueChars, start, end);
+        std::cout << findMaxUnigueChars(str, word, maxWord, maxUnigueChars, start, end);
     }
    catch(std::exception e) {
         std::cout << e.what();
-   }
-      return 0;
+    }
+    return 0;
 }
 
